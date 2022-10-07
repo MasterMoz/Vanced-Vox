da67ce8911dba6336c5ff5d6abb1251d135afa21
Namekujilsds <okosimasu3@gmail.com>
2022-10-07 15:49:15 +0900

    [add]マニフェストファイルの追加とか

--- script.js
+++ script.js
@@ -2,52 +2,70 @@
 const gameBody = document.body;
 const appBody = document.getElementById('app');
 
+let ccImageHeight
+let ccImageWidth
+
+localStorage.getItem("")
+
 // 設定ボックス
 const settingBoxCreate = document.createElement("div"); //<div id="settingBox"></div>を作成する
-settingBoxCreate.setAttribute("id","settingBox"); //idをsettingBoxにする
+settingBoxCreate.setAttribute("id", "settingBox"); //idをsettingBoxにする
 gameBody.appendChild(settingBoxCreate); //bodyの一番下に挿入
 
 const settingBox = document.getElementById("settingBox"); //<div id="settingBox">をsettingBoxに入れる
 
-let checkBoxCreate = document.createElement("input"); //inputを作成
-checkBoxCreate.setAttribute("id","vCheckBox"); //idをvCheckBoxに
-checkBoxCreate.setAttribute("type","checkbox");
-settingBox.appendChild(checkBoxCreate); //settingBoxについか
+const checkBoxCreate = document.createElement("input"); //inputを作成
+checkBoxCreate.setAttribute("id", "vCheckBox"); //idをvCheckBoxに
+checkBoxCreate.setAttribute("type", "checkbox");
+settingBox.appendChild(checkBoxCreate); //settingBoxにadd
 
-let urlInputLabelCreate = document.createElement("label");
-urlInputLabelCreate.setAttribute("value","URL");
-urlInputLabelCreate.setAttribute("id","urlLabel");
+//TEXT [URL]
+const urlInputLabelCreate = document.createElement("label");
+urlInputLabelCreate.setAttribute("value", "URL");
+urlInputLabelCreate.setAttribute("id", "urlLabel");
 urlInputLabelCreate.textContent = "URL";
 settingBox.appendChild(urlInputLabelCreate);
-
 const urlLabel = document.getElementById("urlLabel");
 
-let urlInputCreate = document.createElement("input");
-urlInputCreate.setAttribute("id","urlBox");
-urlInputCreate.setAttribute("type","text");
+//URL BOX
+const urlInputCreate = document.createElement("input");
+urlInputCreate.setAttribute("id", "urlBox");
+urlInputCreate.setAttribute("type", "text");
 urlLabel.appendChild(urlInputCreate);
 
+//Width slider
+const widthSlideCreate = document.createElement("input");
+widthSlideCreate.setAttribute("id", "chWidth");
+widthSlideCreate.setAttribute("type", "slider");
+widthSlideCreate.defaultValue = ccImageWidth;
+
+//Height Slider
+const heightSlideCreate = document.createElement("input");
+heightSlideCreate.setAttribute("id", "chWidth");
+heightSlideCreate.setAttribute("type", "slider");
+heightSlideCreate.defaultValue = ccImageHeight;
 
 // クロスヘア
-const crosshairCreate = createElement("div");
-crosshairCreate.setAttribute("id","crosshair");
+const crosshairCreate = document.createElement("div");
+crosshairCreate.setAttribute("id", "crosshair");
 appBody.appendChild(crosshairCreate);
 
 let crosshairWidth = getComputedStyle(gameBody).getPropertyValue("--ch-width");
 let crosshairHeight = getComputedStyle(gameBody).getPropertyValue("--ch-height");
 // クロスヘア用コード
 window.imgSetClick = () => {
-    const ccImage = document.getElementById('crosshair');
     const setBtn = document.getElementById('setButton');
     const ccImageUrl = document
         .getElementById('urlBox')
         .value;
-    const ccImageWidth = document
+    ccImageWidth = document
         .getElementById('chWidth')
         .value;
-    const ccImageHeight = document
+    localStorage.setItem.cImgW = ccImageWidth;
+    ccImageHeight = document
         .getElementById('chHeight')
         .value;
+    localStorage.setItem.cImgH = ccImageHeight;
     ccImage.innerHTML = `<img src="${ccImageUrl}" width="${ccImageWidth}" height="${ccImageHeight}"/>`;
     // テンプレートリテラル:
     // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Template_literals
@@ -55,9 +73,9 @@ window.imgSetClick = () => {
 
 //style
 
-const style =`<style type="text/css">
-@import url("style.css");
+const style = `<style type="text/css">
+@import url("/style/menu-style.css");
 @import url("https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap");
 </style>`
 
-gameBody.insertAdjacentHTML('beforeend',style);
+gameBody.insertAdjacentHTML('beforeend', style);
