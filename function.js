f31db1ae436fc0629a0e1d382714f944fc3066fd
NamekujiLSDs <okosimasu3@gmail.com>
2022-10-13 14:09:33 +0900

    a

--- function.js
+++ function.js
@@ -1,138 +1,149 @@
 //変数を指定
-const chHolder = document.getElementById('crosshair');
-const hide1 = document.getElementById('title');
-const hide2 = document.getElementById('crosshairSettingBox');
-const hide3 = document.getElementById('settingBox');
-const hide4 = document.getElementById('logoHolder');
+const chHolder = document.getElementById("crosshair");
+const hide1 = document.getElementById("title");
+const hide2 = document.getElementById("crosshairSettingBox");
+const hide3 = document.getElementById("settingBox");
+const hide4 = document.getElementById("logoHolder");
 
-cBoxVal = localStorage.getItem('checkBoxSaved');
+cBoxVal = localStorage.getItem("checkBoxSaved");
 
 //表示非表示チェックボックス
 
-function checkBox(){
-    const checkbox = document.getElementById('vCheckBox');
-    let checktf = checkbox.checked;
-    if(checktf){
-        localStorage.setItem('checkBoxSaved','true');
-        console.log("true");
-        hide1.setAttribute("class","");
-        hide2.setAttribute('class','');
-        hide3.setAttribute('class','');
-        hide4.setAttribute('class','');
-    }else if(!checktf){
-        localStorage.setItem('checkBoxSaved','false');
-        console.log("false");
-        hide1.setAttribute("class","hiddenClass");
-        hide2.setAttribute('class','hiddenClass');
-        hide3.setAttribute('class','hiddenClassSetting');
-        hide4.setAttribute('class','hiddenClass');
-    }
+function checkBox() {
+  const checkbox = document.getElementById("vCheckBox");
+  let checktf = checkbox.checked;
+  if (checktf) {
+    localStorage.setItem("checkBoxSaved", "true");
+    console.log("true");
+    hide1.setAttribute("class", "");
+    hide2.setAttribute("class", "");
+    hide3.setAttribute("class", "");
+    hide4.setAttribute("class", "");
+  } else if (!checktf) {
+    localStorage.setItem("checkBoxSaved", "false");
+    console.log("false");
+    hide1.setAttribute("class", "hiddenClass");
+    hide2.setAttribute("class", "hiddenClass");
+    hide3.setAttribute("class", "hiddenClassSetting");
+    hide4.setAttribute("class", "hiddenClass");
+  }
 }
 
-
-    // window.onload = onloadLogo()
-    function onloadLogo(){
-        let logoTextVal = localStorage.getItem('logoTextSaved');
-        let logoUrlVal = localStorage.getItem('logoUrlSaved');
-        const logoText = document.getElementsByClassName('yYlig')[0];
-        logoText.textContent = logoTextVal;
-        const logo = document.getElementsByClassName('hrxbol')[0];
-        logo.setAttribute('src',logoUrlVal);    
-    }
-
+// window.onload = onloadLogo()
+function onloadLogo() {
+  let logoTextVal = localStorage.getItem("logoTextSaved");
+  let logoUrlVal = localStorage.getItem("logoUrlSaved");
+  const logoText = document.getElementsByClassName("yYlig")[0];
+  logoText.textContent = logoTextVal;
+  const logo = document.getElementsByClassName("hrxbol")[0];
+  logo.setAttribute("src", logoUrlVal);
+}
 
 //設定ボックスの表示・非表示を切り替える
 
-vcheck = document.getElementById('vCheckBox');
+vcheck = document.getElementById("vCheckBox");
 
 window.onload = cbLoaded();
 
-function cbLoaded(){
-    if(cBoxVal === "true"){
-        console.log(cBoxVal +'/ true check');
-        vcheck.checked = true;
-        console.log("default is true");
-        hide1.setAttribute("class","");
-        hide2.setAttribute('class','');
-        hide3.setAttribute('class','');
-        hide4.setAttribute('class','');
-    }else if(cBoxVal === "false"){
-        console.log(cBoxVal +'/ false check');
-        vcheck.checked = false;
-        console.log("default is false");
-        hide1.setAttribute("class","hiddenClass");
-        hide2.setAttribute('class','hiddenClass');
-        hide3.setAttribute('class','hiddenClassSetting');
-        hide4.setAttribute('class','hiddenClass');
-    }
+function cbLoaded() {
+  if (cBoxVal === "true") {
+    console.log(cBoxVal + "/ true check");
+    vcheck.checked = true;
+    console.log("default is true");
+    hide1.setAttribute("class", "");
+    hide2.setAttribute("class", "");
+    hide3.setAttribute("class", "");
+    hide4.setAttribute("class", "");
+  } else if (cBoxVal === "false") {
+    console.log(cBoxVal + "/ false check");
+    vcheck.checked = false;
+    console.log("default is false");
+    hide1.setAttribute("class", "hiddenClass");
+    hide2.setAttribute("class", "hiddenClass");
+    hide3.setAttribute("class", "hiddenClassSetting");
+    hide4.setAttribute("class", "hiddenClass");
+  }
 }
-
 
 //width slider
 window.onload = imgWSet();
 
 //スライダー側
-function imgWSet(){
-    const widthVal = document.getElementById('chWidth').value;
-    document.getElementById('widthVal').value = widthVal;
-    ccImageWidth = document.getElementById('chWidth').value;
-    chHolder.setAttribute('width',widthVal +'px');
-    localStorage.setItem('crosshairWidthSaved',widthVal);
+function imgWSet() {
+  const widthVal = document.getElementById("chWidth").value;
+  document.getElementById("widthVal").value = widthVal;
+  ccImageWidth = document.getElementById("chWidth").value;
+  chHolder.setAttribute("width", widthVal + "px");
+  localStorage.setItem("crosshairWidthSaved", widthVal);
 }
 //numberのinput側
-function imgWSet1(){
-    const widthVal = document.getElementById('widthVal').value;
-    chHolder.setAttribute('width',widthVal +'px');
-    document.getElementById('chWidth').value = widthVal;
+function imgWSet1() {
+  const widthVal = document.getElementById("widthVal").value;
+  chHolder.setAttribute("width", widthVal + "px");
+  document.getElementById("chWidth").value = widthVal;
 }
-
 
 //height slider
-window.onload = imgHSet()
+window.onload = imgHSet();
 //スライダー側
-function imgHSet(){
-    const heightVal = document.getElementById('chHeight').value;
-    document.getElementById('heightVal').value = heightVal;
-    ccImageHeight = document.getElementById('chHeight').value;
-    chHolder.setAttribute('height',heightVal +'px');
-    localStorage.setItem('crosshairHeightSaved',heightVal);
+function imgHSet() {
+  const heightVal = document.getElementById("chHeight").value;
+  document.getElementById("heightVal").value = heightVal;
+  ccImageHeight = document.getElementById("chHeight").value;
+  chHolder.setAttribute("height", heightVal + "px");
+  localStorage.setItem("crosshairHeightSaved", heightVal);
 }
 //numberのinput側
-function imgHSet1(){
-    const heightVal  = document.getElementById('heightVal').value;
-    chHolder.setAttribute('height',heightVal +'px');
-    //スライダーのvalueを変更
-    document.getElementById('chHeight').value = heightVal;
+function imgHSet1() {
+  const heightVal = document.getElementById("heightVal").value;
+  chHolder.setAttribute("height", heightVal + "px");
+  //スライダーのvalueを変更
+  document.getElementById("chHeight").value = heightVal;
 }
-
 
 //URLを設定
-function imgUrlSet(){
-    const imageUrl = document.getElementById('urlBox').value;
-    chHolder.setAttribute('src',imageUrl);
-    localStorage.setItem('crosshairUrlSaved', imageUrl);
-
+function imgUrlSet() {
+  const imageUrl = document.getElementById("urlBox").value;
+  chHolder.setAttribute("src", imageUrl);
+  localStorage.setItem("crosshairUrlSaved", imageUrl);
 }
-
 
 //auto default size
-function defaultSize(){
-    const crosshairImage = document.getElementById('crosshair');
-    //画像サイズの取得
-    let imgWidth =  crosshairImage.naturalWidth;
-    let imgHeight = crosshairImage.naturalHeight;
-    console.log('Width is ' + imgWidth + 'px');
-    console.log('Height is ' + imgHeight + 'px');
-    //画像サイズ
-    chHolder.setAttribute('width',imgWidth +'px');
-    chHolder.setAttribute('height',imgHeight +'px');
-    //pxの表示
-    document.getElementById('widthVal').value = imgWidth;
-    document.getElementById('heightVal').value = imgHeight;
-    //localstorageに格納
-    localStorage.setItem('crosshairWidthSaved',imgWidth);
-    localStorage.setItem('crosshairHeightSaved',imgHeight);
-    //スライダーのvalueを変更
-    document.getElementById('chWidth').value = imgWidth;
-    document.getElementById('chHeight').value = imgHeight;
+function defaultSize() {
+  const crosshairImage = document.getElementById("crosshair");
+  //画像サイズの取得
+  let imgWidth = crosshairImage.naturalWidth;
+  let imgHeight = crosshairImage.naturalHeight;
+  console.log("Width is " + imgWidth + "px");
+  console.log("Height is " + imgHeight + "px");
+  //画像サイズ
+  chHolder.setAttribute("width", imgWidth + "px");
+  chHolder.setAttribute("height", imgHeight + "px");
+  //pxの表示
+  document.getElementById("widthVal").value = imgWidth;
+  document.getElementById("heightVal").value = imgHeight;
+  //localstorageに格納
+  localStorage.setItem("crosshairWidthSaved", imgWidth);
+  localStorage.setItem("crosshairHeightSaved", imgHeight);
+  //スライダーのvalueを変更
+  document.getElementById("chWidth").value = imgWidth;
+  document.getElementById("chHeight").value = imgHeight;
 }
+
+//observ
+let target = document.getElementById("app");
+
+const MutObs = new MutationObserver((mutationsList, observer) => {
+  for (const change of mutationsList) {
+    if (change) {
+      console.log(change.type);
+      console.log(change.addedNodes);
+    }
+  }
+});
+
+const options = {
+  subtree: true,
+  childList: true,
+};
+MutObs.observe(target, options);
